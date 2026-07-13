798885595db1644566a701e9a8798db48cdda475
Anh Quoc Nguyen <naquoc17.uni@gmail.com>
2026-07-13 18:35:56 +0200

    L07: add service-restart form (New-UDForm + Sync-UDElement + toast feedback)

--- .universal/Apps/Ops-Console.ps1
+++ .universal/Apps/Ops-Console.ps1
@@ -1,11 +1,24 @@
 New-UDApp -Title 'Ops Console' -Content {
+  New-UDForm -Children {
+    New-UDTextbox -Id 'ServiceName' -Label 'Enter name of the service'
+  } -OnSubmit {
+    $name = $EventData.ServiceName
+    try
+    {
+      Restart-Service -Name $name
+      Show-UDToast -Message "Restarted: $name"
+    } catch
+    {
+      Show-UDToast -Message "Failed: $($_.Exception.Message)"
+    }
+  }
 
-  New-UDDynamic -AutoRefresh:$true -AutoRefreshInterval 5 -Content {
+  New-UDDynamic -Id 'ServiceTable' -AutoRefresh:$true -AutoRefreshInterval 3 -Content {
     $services = Get-Service | Select-Object Name, Status, StartType
     New-UDTable -Data $services -Columns @(
       New-UDTableColumn -Property Name -Title 'Name'
       New-UDTableColumn -Property Status -Title 'State'
     )
-
   }
 }
+
